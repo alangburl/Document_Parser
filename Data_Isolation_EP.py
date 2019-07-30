e6349b45d30cac360c2580dce8e0faf28edb5b83
alangburl <39042458+alangburl@users.noreply.github.com>
2019-07-30 18:07:17 -0500

    Added graphing to visualize the variation

--- Data_Isolation_EP.py
+++ Data_Isolation_EP.py
@@ -25,40 +25,36 @@ def parse_data(file_name,target,bound,data_type=int,*kwargs):
     data=sheet.iter_rows()
     #put the data from the spread sheet into a 2D matrix
     matrix=[[row[i].value for i in range(len(row))] for row in data]
+    
     #convert the 2D matrix into a 1D matrix of absolute values to determine
-    #the 20% bound region, will remove all the NoneTypes for only this matrix
-    #also ge the number of actual entries to compare to for the bounding
-    number_of_values=0
-    matrix_1D=[]
-    none_type=bool
-    for t in range(len(matrix)):
-        for u in range(len(matrix[t])):
-            try:
-                matrix_1D.append(abs(target-matrix[t][u]))
-                number_of_values+=1
-            except:
-                none_type=True
+    #the bound region, will remove all the NoneTypes for only this matrix
+    #also get the number of actual entries to compare to for the bounds
+    matrix_1D=[abs(target-d) for u in matrix for d in u if type(d)==data_type]
+    number_of_values=len(matrix_1D)
+    
     #sort the 1D matrix into ascending order and get rid of the final 20%
     #this might remove 1 value less than desired. IF this is an issue
     #change math.trunc() to int()
     matrix_1D_sorted=np.sort(matrix_1D)[:math.trunc(-number_of_values*bound)]
-    #get the maxiumum tolerable index
+    #get the maxiumum tolerable value
     max_abs_value=matrix_1D_sorted[-1]
     
-    #take the absolute value of each elementa and compare it the max tolerance
+    #take the absolute value of each element and compare it the max tolerance
+    #form a 2D matrix with absolute values for use in plotting it later
+    matrix_2D=matrix.copy()
     for j in range(len(matrix)):
         for k in range(len(matrix[j])):
-            try:
+            if type(matrix[j][k])==data_type:
                 if abs(matrix[j][k]-target)>max_abs_value:
                     matrix[j][k]=None
-            except:
-                none_type=True
-    
-    #create a new sheet in the excel workbook
-    #first check to see if it has already been ran and make a different
-    #sheet using a different name
+                    matrix_2D[j][k]=np.nan
+                else:
+                    matrix_2D[j][k]=abs(matrix[j][k]-target)
+            else:
+                matrix_2D[j][k]=np.nan
+                    
+#    create a new sheet in the excel workbook
     base_name='Parsed_Data_{}_{}'.format(target,bound)
-
     book.create_sheet(base_name)
     new_sheet=book[base_name]
     #write the data to the spread sheet skipping all the instances 
@@ -70,5 +66,14 @@ def parse_data(file_name,target,bound,data_type=int,*kwargs):
     #saving the file
     book.save(file_name+'.xlsx')
     book.close()
-    return 1 
-parse_data('RAW_DATA',250,0.2)
+    return matrix_2D
+
+a=parse_data('RAW_DATA',250,0.2)
+
+fig = plt.figure(figsize=(10, 10))
+plt.imshow(a,interpolation='None')
+plt.title('Variance from target')
+plt.xlabel('Row')
+plt.ylabel('Column')
+plt.colorbar()
+plt.show()
